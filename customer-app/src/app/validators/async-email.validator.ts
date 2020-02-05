38120f856649925e33576e1806684a3cd2133a39
Anmol Sinha <anmol.sinha1993@gmail.com>
2020-02-06 01:19:23 +0530

    removed console

--- customer-app/src/app/validators/async-email.validator.ts
+++ customer-app/src/app/validators/async-email.validator.ts
@@ -8,7 +8,6 @@ import { map, switchMap, filter  } from 'rxjs/operators';
 export class ValidateEmailNotTaken {
     static createValidator(signupService: SignupService) {
       return (control: AbstractControl) => {
-          console.log(control.value)
         return signupService.checkEmailNotTaken(control.value)
             .pipe(
                 map((res) => {
